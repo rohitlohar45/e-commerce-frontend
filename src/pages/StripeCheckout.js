6fac739ef0312432704d67572b60b621bfe4d06a
Rohit <loharrohit9860@gmail.com>
2023-07-09 18:22:58 +0530

    Update

--- src/pages/StripeCheckout.js
+++ src/pages/StripeCheckout.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
 import CheckoutForm from "./CheckoutForm";
 import "../Stripe.css";
@@ -10,39 +10,44 @@ import { selectCurrentOrder } from "../features/order/orderSlice";
 // Make sure to call loadStripe outside of a component’s render to avoid
 // recreating the Stripe object on every render.
 // This is your test publishable API key.
-const stripePromise = loadStripe("pk_test_51N5NLVSF2Mo4AGVvYGMMNnTcPA5lHrkUv8zPymsKfL31c8m6et8525Y92zMgA22m1u6ZkkXsBofre0PJL6i3eVsh00sR6M8c00");
+const stripePromise = loadStripe(
+	"pk_test_51N5NLVSF2Mo4AGVvYGMMNnTcPA5lHrkUv8zPymsKfL31c8m6et8525Y92zMgA22m1u6ZkkXsBofre0PJL6i3eVsh00sR6M8c00"
+);
 
 export default function StripeCheckout() {
-  const [clientSecret, setClientSecret] = useState("");
-  const currentOrder = useSelector(selectCurrentOrder)
+	const [clientSecret, setClientSecret] = useState("");
+	const currentOrder = useSelector(selectCurrentOrder);
 
-  useEffect(() => {
-    // Create PaymentIntent as soon as the page loads
-    fetch("/create-payment-intent", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ totalAmount: currentOrder.totalAmount, orderId:currentOrder.id }),
-    
-    })
-      .then((res) => res.json())
-      .then((data) => setClientSecret(data.clientSecret));
-  }, []);
+	useEffect(() => {
+		// Create PaymentIntent as soon as the page loads
+		let totalAmount = currentOrder.totalAmount;
+		if (NaN(currentOrder.totalAmount)) {
+			totalAmount = 0;
+		}
+		fetch("/create-payment-intent", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ totalAmount: totalAmount, orderId: currentOrder.id }),
+		})
+			.then((res) => res.json())
+			.then((data) => setClientSecret(data.clientSecret));
+	}, []);
 
-  const appearance = {
-    theme: 'stripe',
-  };
-  const options = {
-    clientSecret,
-    appearance,
-  };
+	const appearance = {
+		theme: "stripe",
+	};
+	const options = {
+		clientSecret,
+		appearance,
+	};
 
-  return (
-    <div className="Stripe">
-      {clientSecret && (
-        <Elements options={options} stripe={stripePromise}>
-          <CheckoutForm />
-        </Elements>
-      )}
-    </div>
-  );
+	return (
+		<div className="Stripe">
+			{clientSecret && (
+				<Elements options={options} stripe={stripePromise}>
+					<CheckoutForm />
+				</Elements>
+			)}
+		</div>
+	);
 }
